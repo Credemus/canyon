f231c4978d4957c670c481a3574a731129b002a1
Bodo Junglas <junglas@objectcode.de>
2020-11-24 11:09:34 +0100

    Unwind the knot

--- build-compat.sh
+++ build-compat.sh
@@ -5,4 +5,4 @@ rm -rf src/ejb/generate/
 rm -rf src/jmx/generate/
 rm -rf src/wetdock/generate/
 
-docker run --rm -v $(pwd):/project -w /project -u $(id -u):$(id -g) -e JAVA_HOME=/usr/lib/jvm/java-6-oracle caninjas/jdk6 ./build.sh flow-application-package
+docker run --rm -v $(pwd):/project -w /project -u $(id -u):$(id -g) -e JAVA_HOME=/usr/lib/jvm/java-6-oracle caninjas/jdk6 ./build.sh clean flow-application-package
